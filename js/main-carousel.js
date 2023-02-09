e8c0afca84f3c50b019a74839f595cf145c93f37
katamarang11 <arestov.maks@mail.ru>
2023-02-10 00:07:07 +0300

    Комментарий к ветке Experiments

--- js/main-carousel.js
+++ js/main-carousel.js
@@ -7,6 +7,47 @@ let sliderList = document.querySelector('.slider-list');
 sliderList = sliderList.children;
 
 for (let i = 0; i < sliderControlsBtn.length; i++) {
+
+  setInterval(() => {
+    [...sliderList].map(elem => {
+      return elem.classList.add('non');
+    });
+    sliderList[1].classList.remove('non');
+
+    [...insideTheBtn].map(elem => {
+      return elem.classList.add('none-btn');
+    });
+    insideTheBtn[1].classList.remove('none-btn');
+
+    setTimeout(() => {
+      [...sliderList].map(elem => {
+        return elem.classList.add('non');
+      });
+      sliderList[2].classList.remove('non');
+
+      [...insideTheBtn].map(elem => {
+        return elem.classList.add('none-btn');
+      });
+      insideTheBtn[2].classList.remove('none-btn');
+
+      setTimeout(() => {
+        [...sliderList].map(elem => {
+          return elem.classList.add('non');
+        });
+        sliderList[0].classList.remove('non');
+
+        [...insideTheBtn].map(elem => {
+          return elem.classList.add('none-btn');
+        });
+        insideTheBtn[0].classList.remove('none-btn');
+      }, 4000);
+
+    }, 4000);
+
+  }, 10000);
+
+
+
   sliderControlsBtn[i].addEventListener('click', function () {
     [...sliderList].map(elem => {
       return elem.classList.add('non');
@@ -19,4 +60,3 @@ for (let i = 0; i < sliderControlsBtn.length; i++) {
     insideTheBtn[i].classList.remove('none-btn');
   });
 }
-
