e1ea5065c80506ec55659333f1dbd31a6f3c7c52
katamarang11 <arestov.maks@mail.ru>
2023-02-12 20:49:55 +0300

    Added animation

--- js/main-pop-up.js
+++ js/main-pop-up.js
@@ -4,15 +4,18 @@ const modalWindow = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close-btn');
 
 mapContactsBtn.addEventListener('click', function () {
+  modalWindow.classList.remove('close-window');
   modalWindow.classList.remove('none');
 });
 
 closeBtn.addEventListener('click', function () {
-  modalWindow.classList.add('none');
+  modalWindow.classList.add('close-window');
+  setTimeout(() => modalWindow.classList.add('none'), 1000);
 });
 
 document.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    modalWindow.classList.add('none');
+    modalWindow.classList.add('close-window');
+    setTimeout(() => modalWindow.classList.add('none'), 1000);
   }
 });
